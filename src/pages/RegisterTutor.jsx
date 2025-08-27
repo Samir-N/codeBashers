5ec62bdc61b352faf567c21ed1e7cfa92eb83e5e
Samir Niroula <samirniroula79@gmail.com>
2025-08-27 12:42:30 +0545

    filemade

---
+++ src/pages/RegisterTutor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterTutor = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterTutor
---
+++ src/pages/studentDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const studentDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default studentDashboard
